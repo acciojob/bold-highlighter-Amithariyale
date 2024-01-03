27012f1b9ee69db3d250388f3d8151c3f00778ee
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-03 16:04:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const boldItems=document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
-
+	boldItems.forEach(item=>item.style.color='green');
+	
 }
 
 
 function return_normal() {
     //Write your code here
 
-    
+    boldItems.forEach(item=>item.style.color='black');
 }
